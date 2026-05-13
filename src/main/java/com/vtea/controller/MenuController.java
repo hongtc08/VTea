c45c426f555a62e2fe8242e637a58a179447e47a
Haibrosh <haibrosh5c@gmail.com>
2026-05-13 09:52:08 +0700

    fix: menu navigation

--- src/main/java/com/vtea/controller/MenuController.java
+++ src/main/java/com/vtea/controller/MenuController.java
@@ -6,4 +6,19 @@ public class MenuController {
     public void handleAddNewDish(ActionEvent actionEvent) {
 
     }
+
+    public void filterAll(ActionEvent actionEvent) {
+    }
+
+    public void filterTraSua(ActionEvent actionEvent) {
+    }
+
+    public void filterCafe(ActionEvent actionEvent) {
+    }
+
+    public void filterTra(ActionEvent actionEvent) {
+    }
+
+    public void filterDacBiet(ActionEvent actionEvent) {
+    }
 }
